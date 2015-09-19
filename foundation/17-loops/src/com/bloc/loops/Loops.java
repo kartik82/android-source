58c10fca5fa9774747eea740d6f1f635dfdeba42
Kartik Shah <kartik82@gmail.com>
2015-09-19 17:28:07 +0300

    Checkpoint 17 completed

--- foundation/17-loops/src/com/bloc/loops/Loops.java
+++ foundation/17-loops/src/com/bloc/loops/Loops.java
@@ -11,21 +11,16 @@ public class Loops extends Object {
 		boolean temp = false;
 
 		//********** Starts Here ************************/
-		temp = someBools[7];
-		someBools[7] = someBools[0];
-		someBools[0] = temp;
+		int i = 7;
+		int j = 0;
 
-		temp = someBools[6];
-		someBools[6] = someBools[1];
-		someBools[1] = temp;
-
-		temp = someBools[5];
-		someBools[5] = someBools[2];
-		someBools[2] = temp;
-
-		temp = someBools[4];
-		someBools[4] = someBools[3];
-		someBools[3] = temp;
+		while (i > 3) {
+			temp = someBools[i];
+			someBools[i] = someBools[j];
+			someBools[j] = temp;
+			i--;
+			j++;
+		}
 		//********** Ends Here **************************/
 		
 
@@ -50,14 +45,9 @@ public class Loops extends Object {
 		/************************************************/
 
 		//********** Starts Here ************************/
-		numArray[0] = !someBools[0] ? 1 : 0;
-		numArray[1] = !someBools[1] ? 1 : 0;
-		numArray[2] = !someBools[2] ? 1 : 0;
-		numArray[3] = !someBools[3] ? 1 : 0;
-		numArray[4] = !someBools[4] ? 1 : 0;
-		numArray[5] = !someBools[5] ? 1 : 0;
-		numArray[6] = !someBools[6] ? 1 : 0;
-		numArray[7] = !someBools[7] ? 1 : 0;
+		for (int x = 0; x < 8; x++) {
+			numArray[x] = !someBools[x] ? 1 : 0;
+		}
 		//********** Ends Here **************************/
 
 
