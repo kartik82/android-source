516bd9359b2ea5d751d85f221759bb63ce2684a3
Kartik Shah <kartik82@gmail.com>
2015-10-03 14:41:12 +0300

    Checkpoint 29 completed

--- foundation/29-exceptions/src/com/bloc/exceptions/Main.java
+++ foundation/29-exceptions/src/com/bloc/exceptions/Main.java
@@ -33,16 +33,26 @@ public class Main extends Object {
 	/************************************************/
 	private static final void tryGetMax() {
 		int max = 0;
-		max = FunMethods.getMax((Integer[])null);
+
+		try {
+			max = FunMethods.getMax((Integer[])null);	
+		} catch (IllegalArgumentException e) {
+			// do nothing
+		}
+
 		Integer[] numbers = new Integer[50];
 		Random rand = new Random();
 		for (int i = 0; i < 50; i++) {
 			numbers[i] = new Integer(rand.nextInt(500));
 		}
-		numbers[32] = null;
-		max = FunMethods.getMax(numbers);
-		numbers[32] = new Integer(rand.nextInt(500));
-		max = FunMethods.getMax(numbers);
+
+		try {
+			numbers[32] = null;
+			max = FunMethods.getMax(numbers);			
+		} catch (IllegalStateException e) {
+			numbers[32] = new Integer(rand.nextInt(500));
+			max = FunMethods.getMax(numbers);
+		}
 	}
 
 	/************************************************
@@ -50,7 +60,13 @@ public class Main extends Object {
  	 *	Catch thrown exceptions
 	/************************************************/
 	private static final void tryRemove() {
-		FunMethods.remove(null, 2);
+
+		try {
+			FunMethods.remove(null, 2);
+		} catch (IllegalArgumentException e) {
+			// do nothing
+		}
+		
 		Object[] someObjects = new Object[12];
 		someObjects[0] = "a string!";
 		someObjects[1] = new Integer(32);
@@ -59,7 +75,13 @@ public class Main extends Object {
 		for (int i = 4; i < someObjects.length; i++) {
 			someObjects[i] = String.valueOf(i);
 		}
-		FunMethods.remove(someObjects, 12);
+
+		try {
+			FunMethods.remove(someObjects, 12);
+		} catch (IndexOutOfBoundsException e) {
+			// do nothing
+		}
+
 		someObjects = FunMethods.remove(someObjects, 3);
 	}
 }
