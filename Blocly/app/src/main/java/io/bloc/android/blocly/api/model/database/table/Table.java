b056a1c067f5c5feaa8c24a3e310949bd3699e97
Kartik Shah <kartik82@gmail.com>
2015-10-15 02:33:21 +0300

    Checkpoint 52 completed

---
+++ Blocly/app/src/main/java/io/bloc/android/blocly/api/model/database/table/Table.java
@@ -0,0 +1,19 @@
+package io.bloc.android.blocly.api.model.database.table;
+
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Kartik on 15-Oct-15.
+ */
+public abstract class Table {
+
+    protected static final String COLUMN_ID = "id";
+
+    public abstract String getName();
+
+    public abstract String getCreateStatement();
+
+    public void onUpgrade(SQLiteDatabase writableDatabase, int oldVersion, int newVersion) {
+        // Nothing
+    }
+}
